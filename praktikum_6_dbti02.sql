41bc3038384ba19a975942fa62c876af6b68efdf
Eko Muchamad Haryono <66320809+ekomh170@users.noreply.github.com>
2024-04-02 00:35:08 +0700

    1.6. Menambahkan Update Kondisi Database Pada Praktikum 6

---
+++ praktikum_6_dbti02.sql
@@ -0,0 +1,110 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 01 Apr 2024 pada 19.26
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbti02`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `barang`
+--
+
+CREATE TABLE `barang` (
+  `id` int(11) NOT NULL,
+  `kode` char(5) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `harga` double NOT NULL,
+  `stok` int(11) NOT NULL,
+  `foto` varchar(30) DEFAULT NULL,
+  `idjenis` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `barang`
+--
+
+INSERT INTO `barang` (`id`, `kode`, `nama`, `harga`, `stok`, `foto`, `idjenis`) VALUES
+(1, 'TV01', 'TV SHARP 60 INCH', 2000000, 10, 'tv.jpg', 1),
+(2, 'AC01', 'AC Panasonic 2 PK', 5000000, 10, NULL, 1),
+(3, 'MJ02', 'Meja Belajar', 400000, 50, NULL, 2),
+(4, 'AG03', 'Ayam Geprek', 15000, 100, NULL, 3),
+(6, 'ET04', 'Es Teh', 5000, 120, NULL, 4),
+(7, 'KO11', 'Kopi Pahit', 10000, 50, NULL, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `jenis`
+--
+
+CREATE TABLE `jenis` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `jenis`
+--
+
+INSERT INTO `jenis` (`id`, `nama`) VALUES
+(1, 'Elektronik'),
+(2, 'Furniture'),
+(3, 'Makanan'),
+(4, 'Minuman');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `kode` (`kode`);
+
+--
+-- Indeks untuk tabel `jenis`
+--
+ALTER TABLE `jenis`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `nama` (`nama`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `barang`
+--
+ALTER TABLE `barang`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `jenis`
+--
+ALTER TABLE `jenis`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
